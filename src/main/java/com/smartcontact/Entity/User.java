f559a1d8f5422bbc96f2e314f7fe5edaed02ff71
Suman Bej <sumanbej.glt@gmail.com>
2025-08-02 21:15:01 +0530

    added User entity

---
+++ src/main/java/com/smartcontact/Entity/User.java
@@ -0,0 +1,28 @@
+package com.smartcontact.Entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "Users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String name;
+    @Column(unique = true)
+    private String email;
+    private String password;
+    @Column(unique = true, length = 10, nullable = false)
+    private long mobile;
+    private String role;
+    @Column(length = 500)
+    private String about;
+    private String imageUrl;
+    private boolean enabled;
+}
