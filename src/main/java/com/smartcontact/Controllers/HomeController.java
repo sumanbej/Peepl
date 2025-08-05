8b2c673e7e6ab6f81c1a386291e0f3f741826dbc
Suman Bej <sumanbej.glt@gmail.com>
2025-08-05 23:04:14 +0530

    adding other page controller

--- src/main/java/com/smartcontact/Controllers/HomeController.java
+++ src/main/java/com/smartcontact/Controllers/HomeController.java
@@ -4,18 +4,35 @@ import com.smartcontact.Entity.User;
 import com.smartcontact.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
 
-    @Autowired
-    private UserRepository userRepository;
 
     @GetMapping("/home")
-
-    public String test(){
+    public String home(Model model){
+        model.addAttribute("title", "Home - Peepl");
         return "home";
     }
+    @GetMapping("/about")
+    public String about(Model model){
+        model.addAttribute("title", "About - Peepl");
+        return "about";}
+    @GetMapping("/contact")
+    public String contact(Model model) {
+        model.addAttribute("title", "Contact - Peepl");
+        return "contact";
+    }
+    @GetMapping("/login")
+    public String login(Model model) {
+        model.addAttribute("title", "Login - Peepl");
+        return "login";
+    }
+    @GetMapping("/register")
+    public String register(Model model) {
+        model.addAttribute("title", "Register - Peepl");
+        return "register";}
 }
