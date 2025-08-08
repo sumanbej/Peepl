f6e8abe5cd93aef159bd198af74a0432d0d6ca58
Suman Bej <sumanbej.glt@gmail.com>
2025-08-08 23:47:12 +0530

    DTO added for register

---
+++ src/main/java/com/smartcontact/DTO/RegistrationRequest.java
@@ -0,0 +1,16 @@
+package com.smartcontact.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegistrationRequest {
+    private String name;
+    private String email;
+    private String username;
+    private String password;
+    private String confirmPassword;
+}
