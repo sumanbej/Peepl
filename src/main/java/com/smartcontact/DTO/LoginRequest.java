dc9caad75ed3f15742f03ab0568e6b598d87b714
Suman Bej <sumanbej.glt@gmail.com>
2025-08-14 20:20:26 +0530

    login DTO add

---
+++ src/main/java/com/smartcontact/DTO/LoginRequest.java
@@ -0,0 +1,11 @@
+package com.smartcontact.DTO;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class LoginRequest {
+    private String email;
+    private String password;
+}
