f702ce53f72bd29b1744b3359fbc2e366e0360e9
Suman Bej <sumanbej.glt@gmail.com>
2025-09-18 21:11:28 +0530

    controller repo

--- src/main/java/com/smartcontact/Repository/ContactRepository.java
+++ src/main/java/com/smartcontact/Repository/ContactRepository.java
@@ -1,8 +1,7 @@
 package com.smartcontact.Repository;
 
 import com.smartcontact.Entity.Contact;
-import com.smartcontact.Entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ContactRepository extends JpaRepository<User, Contact> {
+public interface ContactRepository extends JpaRepository<Contact, Integer> {
 }
