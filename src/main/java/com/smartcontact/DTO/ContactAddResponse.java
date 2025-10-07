0a117c0d0dc76a26a71b5b1efb35a977b10d44d5
Suman Bej <sumanbej.glt@gmail.com>
2025-10-07 21:50:07 +0530

    conatct response

--- src/main/java/com/smartcontact/DTO/ContactAddResponse.java
+++ src/main/java/com/smartcontact/DTO/ContactAddResponse.java
@@ -3,6 +3,7 @@ package com.smartcontact.DTO;
 public class ContactAddResponse {
     private Long id;
     private String message;
+    private String imageUrl;
 
     // Getters and setters
     public Long getId() { return id; }
@@ -10,4 +11,6 @@ public class ContactAddResponse {
 
     public String getMessage() { return message; }
     public void setMessage(String message) { this.message = message; }
+    public String getImageUrl() { return imageUrl; }
+    public void setImageUrl(String imageUrl) { this.imageUrl = imageUrl; }
 }
